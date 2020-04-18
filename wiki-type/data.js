3d91ef4c7f7eb59a61065c088ee20dcabaf2c91b
wwwcomy <wwwcomy@gmail.com>
2020-04-18 15:47:39 +0800

    add missing files

---
+++ wiki-type/data.js
@@ -0,0 +1,34 @@
+var articles = {
+	"PinkA": [{
+		"title": "Arif Goes Shopping",
+		"content": "Mum puts apples in the trolley. Mum puts oranges in the trolley. Mum puts milk in the trolley. Mum puts juice in the trolley. Mum puts rice in the trolley. Mum puts pizza in the trolley. Mum puts me in the trolley!"
+	}, {
+		"title": "Games",
+		"content": "I like playing marbles. I like playing football. I like playing jump rope. I like playing chess. I like playing hopscotch. I like playing dress-up. I like playing games."
+	}, {
+		"title": "Jamila Finds a Friend",
+		"content": "Jamila finds a book. Jamila finds a pencil. Jamila finds a ruler. Jamila finds a ball. Jamila finds a doll. Jamila finds a banana. Jamila finds a friend!"
+	}, {
+		"title": "Please Stop, Sara!",
+		"content": "Sara finds the drum. Please stop, Sara! Sara finds the pot. Please stop, Sara! Sara finds the car. Please stop, Sara! Sara finds the duck. Please stop, Sara! Sara finds the radio. Please stop, Sara! sara finds the television. Please stop, Sara! Sara finds the book. Don't stop, Adam!"
+	}, {
+		"title": "Packing my Bag",
+		"content": "My book is in my bag. My pen is in my bag. My ball is in my bag. My apple is in my bag. My lunch is in my bag. My drink is in my bag. My umbrella is not in my bag!"
+	}, {
+		"title": "A Hot Day",
+		"content": "Mouse jumps in the cool water. Splash! Turtle jumps in the cool water. Splash! Porcupine jumps in the cool water. Splash! Monkey jumps in the cool water. Splash! Lion jumps in the cool water. Splash! Zebra jumps in the cool water. Splash! Hippo jumps in the cool water. Splash!"
+	}, {
+		"title": "Animal Homes",
+		"content": "Look at me. I live in a pond. Look at me. I live in a den. Look at me. I live in a shell. Look at me. I live in a tree. Look at me. I live in a web. Look at me. I live in a cave. Look at me. I live in a hole. "
+	}, {
+		"title": "Water",
+		"content": "Trees need water to grow. Flowers need water to grow. Peppers need water to grow. Bananas need water to grow. Apples need water to grow. Oranges need water to grow. Grapes need water to grow."
+	}, {
+		"title": "The Sun is Up",
+		"content": "The sun shines on the sea. The sun shines on the trees. The sun shines on the farm. The sun shines on the pond. The sun shines on the town. The sun shines on the house. The sun shines on me!"
+	}, ],
+	"PinkB": [{
+		"title": "PinkB Title1",
+		"content": "PinkB content"
+	}]
+}
