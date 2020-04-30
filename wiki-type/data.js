b532e593e49f9bb81767c065b94429e205aaeaf3
wwwcomy <wwwcomy@gmail.com>
2020-04-30 14:47:25 +0800

    Add PinkB

--- wiki-type/data.js
+++ wiki-type/data.js
@@ -1,43 +1,89 @@
 var articles = {
-	"PinkA": [{
-		"title": "Arif Goes Shopping",
-		"content": "Mum puts apples in the trolley. Mum puts oranges in the trolley. Mum puts milk in the trolley. Mum puts juice in the trolley. Mum puts rice in the trolley. Mum puts pizza in the trolley. Mum puts me in the trolley!"
-	}, {
-		"title": "Games",
-		"content": "I like playing marbles. I like playing football. I like playing jump rope. I like playing chess. I like playing hopscotch. I like playing dress-up. I like playing games."
-	}, {
-		"title": "Jamila Finds a Friend",
-		"content": "Jamila finds a book. Jamila finds a pencil. Jamila finds a ruler. Jamila finds a ball. Jamila finds a doll. Jamila finds a banana. Jamila finds a friend!"
-	}, {
-		"title": "Please Stop, Sara!",
-		"content": "Sara finds the drum. Please stop, Sara! Sara finds the pot. Please stop, Sara! Sara finds the car. Please stop, Sara! Sara finds the duck. Please stop, Sara! Sara finds the radio. Please stop, Sara! sara finds the television. Please stop, Sara! Sara finds the book. Don't stop, Adam!"
-	}, {
-		"title": "Packing my Bag",
-		"content": "My book is in my bag. My pen is in my bag. My ball is in my bag. My apple is in my bag. My lunch is in my bag. My drink is in my bag. My umbrella is not in my bag!"
-	}, {
-		"title": "A Hot Day",
-		"content": "Mouse jumps in the cool water. Splash! Turtle jumps in the cool water. Splash! Porcupine jumps in the cool water. Splash! Monkey jumps in the cool water. Splash! Lion jumps in the cool water. Splash! Zebra jumps in the cool water. Splash! Hippo jumps in the cool water. Splash!"
-	}, {
-		"title": "Animal Homes",
-		"content": "Look at me. I live in a pond. Look at me. I live in a den. Look at me. I live in a shell. Look at me. I live in a tree. Look at me. I live in a web. Look at me. I live in a cave. Look at me. I live in a hole. "
-	}, {
-		"title": "Water",
-		"content": "Trees need water to grow. Flowers need water to grow. Peppers need water to grow. Bananas need water to grow. Apples need water to grow. Oranges need water to grow. Grapes need water to grow."
-	}, {
-		"title": "The Sun is Up",
-		"content": "The sun shines on the sea. The sun shines on the trees. The sun shines on the farm. The sun shines on the pond. The sun shines on the town. The sun shines on the house. The sun shines on me!"
-	}, ],
-	"PinkB": [{
-		"title": "My Dad is a Builder",
-		"content": "My dad is a builder. He has some wood. What is my dad building? He has a hammer. What is my dad building? He has a saw. What is my dad building? He has some glue. What is my dad building? He has s paint brush. What is my dad building? What is my dad building? He is building a house for me!"
-	}, {
-		"title": "The Last Lemon",
-		"content": "'I can't get the last lemon,' said the elephant. 'I can help you', said the goat. 'I can get on your back.' 'I can't get the last lemon,' said the goat. 'I can help you,' said the monkey. 'I can get on your back.' 'I can't get the last lemon,' said the monkey. 'I can help you,' said the squirrel. 'I can get on your back.' OUCH! Crash! 'I can get the last lemon!' said the squirrel."
-	}, {
-		"title": "Our Den",
-		"content": "'Look! I've got some blankets for the den,' said Sami. 'Good,' said Max. 'Look! I've got some string for the den,' said Sami. 'Good,' said Max. 'Look! I've got some pegs for the den,' said Sami. 'Good,' said Max. 'Look! I've got a table for the den,' said Sami. 'Good,' said Max. 'Look! I've got some chairs for the den,' said Sami. 'Good,' said Max. 'Look! I've got some food for the den,' said Sami. 'Good,' said Max. 'I've got a den,' said Max. 'Good,' said Sami."
-	}, {
-		"title": "Omar's First Day at School",
-		"content": "Omar is going to school. 'This is my new uniform,' said Omar. 'This is my new school,' said Omar. 'This is my new class,' said Omar. 'This is my new teacher,' said Omar. 'This is my new pencil case,' said Omar. 'This is my new book,' said Omar. 'Beno and Zara are my new friends,' said Omar."
-	}]
+    "PinkA": [{
+        "title": "Arif Goes Shopping",
+        "content": "Mum puts apples in the trolley. Mum puts oranges in the trolley. Mum puts milk in the trolley. Mum puts juice in the trolley. Mum puts rice in the trolley. Mum puts pizza in the trolley. Mum puts me in the trolley!"
+    }, {
+        "title": "Games",
+        "content": "I like playing marbles. I like playing football. I like playing jump rope. I like playing chess. I like playing hopscotch. I like playing dress-up. I like playing games."
+    }, {
+        "title": "Jamila Finds a Friend",
+        "content": "Jamila finds a book. Jamila finds a pencil. Jamila finds a ruler. Jamila finds a ball. Jamila finds a doll. Jamila finds a banana. Jamila finds a friend!"
+    }, {
+        "title": "Please Stop, Sara!",
+        "content": "Sara finds the drum. Please stop, Sara! Sara finds the pot. Please stop, Sara! Sara finds the car. Please stop, Sara! Sara finds the duck. Please stop, Sara! Sara finds the radio. Please stop, Sara! sara finds the television. Please stop, Sara! Sara finds the book. Don't stop, Adam!"
+    }, {
+        "title": "Packing my Bag",
+        "content": "My book is in my bag. My pen is in my bag. My ball is in my bag. My apple is in my bag. My lunch is in my bag. My drink is in my bag. My umbrella is not in my bag!"
+    }, {
+        "title": "A Hot Day",
+        "content": "Mouse jumps in the cool water. Splash! Turtle jumps in the cool water. Splash! Porcupine jumps in the cool water. Splash! Monkey jumps in the cool water. Splash! Lion jumps in the cool water. Splash! Zebra jumps in the cool water. Splash! Hippo jumps in the cool water. Splash!"
+    }, {
+        "title": "Animal Homes",
+        "content": "Look at me. I live in a pond. Look at me. I live in a den. Look at me. I live in a shell. Look at me. I live in a tree. Look at me. I live in a web. Look at me. I live in a cave. Look at me. I live in a hole. "
+    }, {
+        "title": "Water",
+        "content": "Trees need water to grow. Flowers need water to grow. Peppers need water to grow. Bananas need water to grow. Apples need water to grow. Oranges need water to grow. Grapes need water to grow."
+    }, {
+        "title": "The Sun is Up",
+        "content": "The sun shines on the sea. The sun shines on the trees. The sun shines on the farm. The sun shines on the pond. The sun shines on the town. The sun shines on the house. The sun shines on me!"
+    }, ],
+    "PinkB": [{
+        "title": "My Dad is a Builder",
+        "content": "My dad is a builder. He has some wood. What is my dad building? He has a hammer. What is my dad building? He has a saw. What is my dad building? He has some glue. What is my dad building? He has s paint brush. What is my dad building? What is my dad building? He is building a house for me!"
+    }, {
+        "title": "The Last Lemon",
+        "content": "'I can't get the last lemon,' said the elephant. 'I can help you', said the goat. 'I can get on your back.' 'I can't get the last lemon,' said the goat. 'I can help you,' said the monkey. 'I can get on your back.' 'I can't get the last lemon,' said the monkey. 'I can help you,' said the squirrel. 'I can get on your back.' OUCH! Crash! 'I can get the last lemon!' said the squirrel."
+    }, {
+        "title": "Our Den",
+        "content": "'Look! I've got some blankets for the den,' said Sami. 'Good,' said Max. 'Look! I've got some string for the den,' said Sami. 'Good,' said Max. 'Look! I've got some pegs for the den,' said Sami. 'Good,' said Max. 'Look! I've got a table for the den,' said Sami. 'Good,' said Max. 'Look! I've got some chairs for the den,' said Sami. 'Good,' said Max. 'Look! I've got some food for the den,' said Sami. 'Good,' said Max. 'I've got a den,' said Max. 'Good,' said Sami."
+    }, {
+        "title": "Omar's First Day at School",
+        "content": "Omar is going to school. 'This is my new uniform,' said Omar. 'This is my new school,' said Omar. 'This is my new class,' said Omar. 'This is my new teacher,' said Omar. 'This is my new pencil case,' said Omar. 'This is my new book,' said Omar. 'Beno and Zara are my new friends,' said Omar."
+    }, {
+        "title": "Leela can Skate",
+        "content": "I can put on my skates. I can stand up on my skates. I can run on my skates. I can spin on my skates. I can jump on my skates. I can't stop on my skates. Help!"
+    }, {
+        "title": "Where do they Grow?",
+        "content": "Where do oranges grow? Oranges grow on a tree. Where do potatoes grow? Potatoes grow in the ground. Where do bananas grow? Bananas grow on a tree. Where do carrots grow? Carrots grow in the ground. Where do apples grow? Apples grow on a tree. Where do onions grow? Onions grow in the ground. Where do olives grow? Olives grow on a tree."
+    }, {
+        "title": "Who Lays Eggs?",
+        "content": "These animals lay eggs. bird snake butterfly wasp lizard turtle A bird lays eggs. It lays them in a nest. The eggs are safe in the nest. A butterfly lays eggs. It lays them on a leaf. The eggs are safe on the leaf. A snake lays eggs. It lays them in a log. The eggs are safe in the log. A turtle lays eggs. It lays them in the sand. The eggs are safe in the sand. A wasp lays eggs. It lays them in a nest. The eggs are safe in the nest. A lizard lays eggs. It lays them in the soil. The eggs are safe in the soil."
+    }, {
+        "title": "Looking After Animals",
+        "content": "We need to look after animals. Animals need to eat. We give food to the birds. Animals need to drink. we give water to the camels. Animals need to sleep. We give a bed to the donkeys. Animals need to play. We give toys to the cats. Animals need to wash. We give a bath to the elephants. We need to look after animals."
+    }, {
+        "title": "At the Market",
+        "content": "We go to the market to buy dinner. I buy the fish at the market. Mum buys the spices. I buy the vegetables at the market. Mum buys the fruit. I buy the rice at the market. Mum and I buy dinner at the market."
+    }],
+    "Red band": [{
+        "title": "Our Senses",
+        "content": "Five senses. I have five senses. I can see. I can hear. I can smell. I can taste. I can touch. Seeing. Here are my eyes. I see with my eyes. I can see the cars. Hearing. Here are my ears. I hear with my ears. I can hear the music. Smelling. Here is my nose. I smell with my nose. I can smell the flower. Tasting. Here is my tongue. I taste with my tongue. I can taste the ice cream. Touching. Here are my fingers. I touch with my fingers. I can touch the ice. Look at this. Which of your senses will help you?"
+    }, {
+        "title": "",
+        "content": ""
+    }, {
+        "title": "",
+        "content": ""
+    }, {
+        "title": "",
+        "content": ""
+    }, {
+        "title": "",
+        "content": ""
+    }, {
+        "title": "",
+        "content": ""
+    }, {
+        "title": "",
+        "content": ""
+    }, {
+        "title": "",
+        "content": ""
+    }, {
+        "title": "",
+        "content": ""
+    }, {
+        "title": "",
+        "content": ""
+    }]
 }
